7d7d9e88883867941e91af8ecb7429dbef2976f9
Brian Egan <brianjoseph.egan@gmail.com>
2018-09-29 14:08:47 -0700

    Anagrams
    Implentation and tests for anagram detection.

---
+++ Anagrams/AnagramDetector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    public class AnagramDetector
+    {
+        public bool AreAnagrams(string input, string otherInput)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException(nameof(input), "input cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(otherInput))
+            {
+                throw new ArgumentNullException(nameof(otherInput), "otherInput cannot be null or empty");
+            }
+
+            return CharacterMapEquals(BuildCharacterMap(input.ToLower()), BuildCharacterMap(otherInput.ToLower()));
+        }
+
+        private Dictionary<char, int> BuildCharacterMap(string input)
+        {
+            Dictionary<char, int> charMap = new Dictionary<char, int>();
+
+            foreach (char character in input)
+            {
+                if (!char.IsWhiteSpace(character))
+                {
+                    if (charMap.ContainsKey(character))
+                    {
+                        charMap[character]++;
+                    }
+                    else
+                    {
+                        charMap[character] = 1;
+                    }
+                }
+            }
+
+            return charMap;
+        }
+
+        private bool CharacterMapEquals(Dictionary<char, int> map, Dictionary<char, int> otherMap)
+        {
+            return map.Count == otherMap.Count && !map.Except(otherMap).Any();
+        }
+    }
+}
---
+++ Anagrams/AnagramDetectorTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Anagrams
+{
+    [TestFixture]
+    class AnagramDetectorTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            detector = new AnagramDetector();
+        }
+
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => detector.AreAnagrams(null, "test"));
+
+            StringAssert.Contains("input cannot be null or empty", ex.Message);
+            StringAssert.Contains("input", ex.ParamName);
+        }
+
+        [Test]
+        public void OtherNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => detector.AreAnagrams("test", null));
+
+            StringAssert.Contains("otherInput cannot be null or empty", ex.Message);
+            StringAssert.Contains("otherInput", ex.ParamName);
+        }
+
+        [Test]
+        public void Empty()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => detector.AreAnagrams(string.Empty, "test"));
+
+            StringAssert.Contains("input cannot be null or empty", ex.Message);
+            StringAssert.Contains("input", ex.ParamName);
+        }
+
+        [Test]
+        public void OtherEmpty()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => detector.AreAnagrams("test", string.Empty));
+
+            StringAssert.Contains("otherInput cannot be null or empty", ex.Message);
+            StringAssert.Contains("otherInput", ex.ParamName);
+        }
+
+        [TestCase("hello", "llohe")]
+        [TestCase("Whoa! Hi!", "Hi! Whoa!")]
+        public void Positive(string input, string otherInput)
+        {
+            Assert.IsTrue(detector.AreAnagrams(input, otherInput));
+        }
+
+        [TestCase("One One", "Two two two")]
+        [TestCase("One one", "One one c")]
+        [TestCase("A tree, a life, a bench", "A tree, a fence, a yard")]
+        public void Negative(string input, string otherInput)
+        {
+            Assert.IsFalse(detector.AreAnagrams(input, otherInput));
+        }
+
+        private AnagramDetector detector;
+    }
+}
