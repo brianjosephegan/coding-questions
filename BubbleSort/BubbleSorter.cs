142efde8d65932f476308ab5b2822d23dc9a9f1c
Brian Egan <brianjoseph.egan@gmail.com>
2018-09-29 21:04:49 -0700

    Bubble Sort
    Implementation and tests for sorting an array of integers using Bubble Sort algorithm.

---
+++ BubbleSort/BubbleSorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    /// <summary>
+    /// Class to sort integers using Bubble Sort algorithm.
+    /// </summary>
+    public class BubbleSorter
+    {
+        /// <summary>
+        /// Sorts the specified array of integers using Bubble Sort algorithm.
+        /// </summary>
+        /// <param name="numbers">Array of integers to sort.</param>
+        /// <returns>Sorted array of integers.</returns>
+        public int[] Sort(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers), "numbers cannot be null");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        numbers[j] ^= numbers[j + 1];
+                        numbers[j + 1] ^= numbers[j];
+                        numbers[j] ^= numbers[j + 1];
+                    }
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
