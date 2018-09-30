86fdf822a9520f11fc50facc1067f39bb51fd811
Brian Egan <brianjoseph.egan@gmail.com>
2018-09-30 13:36:16 -0700

    Selection Sort
    Implementation and tests for sorting an array of integers using Selection Sort algorithm.

---
+++ SelectionSort/SelectionSorter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    /// <summary>
+    /// Class to sort integers using Selection Sort algorithm.
+    /// </summary>
+    public class SelectionSorter
+    {
+        /// <summary>
+        /// Sorts the specified array of integers using Selection Sort algorithm.
+        /// </summary>
+        /// <param name="numbers">Array of integers to sort.</param>
+        /// <returns>Sorted array of integers.</returns>
+        public int[] Sort(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers), "numbers cannot be null");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[minIndex] > numbers[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    numbers[i] ^= numbers[minIndex];
+                    numbers[minIndex] ^= numbers[i];
+                    numbers[i] ^= numbers[minIndex];
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ SelectionSort/SelectionSorterTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SelectionSort
+{
+    /// <summary>
+    /// Tests for BubbleSorter class.
+    /// </summary>
+    [TestFixture]
+    class SelectionSorterTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            sorter = new SelectionSorter();
+        }
+
+        /// <summary>
+        /// Checks that null is handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => sorter.Sort(null));
+
+            StringAssert.Contains("numbers cannot be null", ex.Message);
+        }
+
+        /// <summary>
+        /// Checks that sequence of numbers can be sorted correctly.
+        /// </summary>
+        /// <param name="expected">Expected array of integers.</param>
+        /// <param name="input">Array of integers to sort.</param>
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { -1 }, new int[] { -1 })]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [TestCase(new int[] { -124, -40, 0, 7, 21, 100, 500 }, new int[] { 100, -40, 500, -124, 0, 21, 7 })]
+        public void Sort(int[] expected, int[] input)
+        {
+            CollectionAssert.AreEqual(expected, sorter.Sort(input));
+        }
+
+        /// <summary>
+        /// Selection sorter.
+        /// </summary>
+        private SelectionSorter sorter;
+    }
+}
