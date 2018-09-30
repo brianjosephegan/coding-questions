f489daa295cefa6cbf134e91a9b31b2ed3a6abcf
Brian Egan <brianjoseph.egan@gmail.com>
2018-09-29 21:35:32 -0700

    Palindromes
    Implentation and tests for palindrome detection.

---
+++ Palindromes/PalindromeDetector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    /// <summary>
+    /// Class to detect a palindrome.
+    /// </summary>
+    public class PalindromeDetector
+    {
+        /// <summary>
+        /// Detects if the specified string is a palindrome. 
+        /// </summary>
+        /// <param name="input">String to check if it is a palindrome.</param>
+        /// <returns>True if string is a palindrome; false otherwise.</returns>
+        public bool IsPalindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input cannot be null", nameof(input));
+            }
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Palindromes/PalindromeDetectorTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Palindromes
+{
+    /// <summary>
+    /// Tests for PalindromeDetector class.
+    /// </summary>
+    [TestFixture]
+    class PalindromeDetectorTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            detector = new PalindromeDetector();
+        }
+
+        /// <summary>
+        /// Checks that null is handled correctly as input.
+        /// </summary>
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => detector.IsPalindrome(null));
+
+            StringAssert.Contains("input cannot be null", ex.Message);
+        }
+
+        /// <summary>
+        /// Positive test cases where the inputs are palindromes.
+        /// </summary>
+        /// <param name="input">String to check if it is a palindrome.</param>
+        [TestCase("aba")]
+        [TestCase("1000000001")]
+        [TestCase("level")]
+        public void Positive(string input)
+        {
+            Assert.IsTrue(detector.IsPalindrome(input));
+        }
+
+        /// <summary>
+        /// Negative test cases where the inputs are not palindromes.
+        /// </summary>
+        /// <param name="input">String to check if it is a palindrome.</param>
+        [TestCase(" aba")]
+        [TestCase("aba ")]
+        [TestCase("greetings")]
+        [TestCase("Fish")]
+        public void Negative(string input)
+        {
+            Assert.IsFalse(detector.IsPalindrome(input));
+        }
+
+        /// <summary>
+        /// Palindrome detector.
+        /// </summary>
+        private PalindromeDetector detector;
+    }
+}
