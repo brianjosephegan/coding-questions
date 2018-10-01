8d90fdbc1ce2290821ecfe1391f6dd094af21d7b
Brian Egan <brianjoseph.egan@gmail.com>
2018-09-30 18:26:04 -0700

    Fizz Buzz
    Implementation and tests for a game of Fizz Buzz.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    /// <summary>
+    /// Class to implement the logic for a game of Fizz Buzz.
+    /// </summary>
+    public class FizzBuzz
+    {
+        /// <summary>
+        /// Prints a game of Fizz Buzz up to the specified number of lines.
+        /// </summary>
+        /// <param name="number">Number of lines to print.</param>
+        public void Print(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException("number cannot be less than or equal to zero", nameof(number));
+            }
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    /// <summary>
+    /// Tests for FizzBuzz class.
+    /// </summary>
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        /// <summary>
+        /// Checks that a negative number of lines is handled correctly.
+        /// </summary>
+        [Test]
+        public void Negative()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => { fizzBuzz.Print(-1); });
+
+            StringAssert.Contains("number cannot be less than or equal to zero", ex.Message);
+        }
+
+        /// <summary>
+        /// Checks that zero number of lines is handled correctly.
+        /// </summary>
+        [Test]
+        public void Zero()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => { fizzBuzz.Print(-1); });
+
+            StringAssert.Contains("number cannot be less than or equal to zero", ex.Message);
+        }
+
+        /// <summary>
+        /// Checks that positive number of lines is handled correctly.
+        /// </summary>
+        /// <param name="number">Number of lines to print.</param>
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(15)]
+        [TestCase(30)]
+        public void Positive(int number)
+        {
+            fizzBuzz.Print(number);
+        }
+
+        private FizzBuzz fizzBuzz;
+    }
+}
