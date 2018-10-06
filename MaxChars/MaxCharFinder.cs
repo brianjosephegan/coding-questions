b7b2a55fae132c426a9961e09109c0e7247039dc
Brian Egan <brianjoseph.egan@gmail.com>
2018-10-05 23:22:11 -0700

    Maximum Characters
    Implementation and tests for finding the highest occurring character in a string.

---
+++ MaxChars/MaxCharFinder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxChars
+{
+    /// <summary>
+    /// Class to find the character that occurs the most in a string.
+    /// </summary>
+    public class MaxCharFinder
+    {
+        /// <summary>
+        /// Finds the character that occurs the most in the specified string.
+        /// </summary>
+        /// <param name="input">String to find the chracter that occurs the most in.</param>
+        /// <returns>Character that appeared the most in the string.</returns>
+        public char? Find(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input cannot be null", nameof(input));
+            }
+
+            char? max = null;
+            Dictionary<char, int> characterMap = new Dictionary<char, int>();
+
+            foreach (char character in input)
+            {
+                if (characterMap.ContainsKey(character))
+                {
+                    characterMap[character]++;
+                    if (characterMap[max.Value] < characterMap[character])
+                    {
+                        max = character;
+                    }
+                }
+                else
+                {
+                    characterMap.Add(character, 1);
+                    if (max == null)
+                    {
+                        max = character;
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ MaxChars/MaxCharFinderTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MaxChars
+{
+    /// <summary>
+    /// Tests for MaxCharFinder class.
+    /// </summary>
+    [TestFixture]
+    class MaxCharFinderTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            finder = new MaxCharFinder();
+        }
+
+        /// <summary>
+        /// Checks that null is handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => finder.Find(null));
+
+            StringAssert.Contains("input cannot be null", ex.Message);
+        }
+
+        /// <summary>
+        /// Checks that empty string is handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Empty()
+        {
+            Assert.AreEqual(null, finder.Find(string.Empty));
+        }
+
+        /// <summary>
+        /// Checks that a single character is handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Character()
+        {
+            Assert.AreEqual('a', finder.Find("a"));
+        }
+
+        /// <summary>
+        /// Checks that a sentence is handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Sentence()
+        {
+            Assert.AreEqual('a', finder.Find("abcdefghijklmnaaaaa"));
+        }
+
+        /// <summary>
+        /// Checks that numbers are handled correctly as an input.
+        /// </summary>
+        [Test]
+        public void Numbers()
+        {
+            Assert.AreEqual('1', finder.Find("ab1c1d1e1f1g1"));
+        }
+
+        /// <summary>
+        /// Maximum character finder.
+        /// </summary>
+        private MaxCharFinder finder;
+    }
+}
