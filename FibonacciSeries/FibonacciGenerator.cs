c603482812fc2f98d7db26183074b61b5f91c48e
Brian Egan <brianjoseph.egan@gmail.com>
2018-10-06 11:10:10 -0700

    Fibonacci Series
    Implementation and tests for generating numbers in the Fibonacci series.

---
+++ FibonacciSeries/FibonacciGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    /// <summary>
+    /// Class to generate the Fibonacci series.
+    /// </summary>
+    public class FibonacciGenerator
+    {
+        /// <summary>
+        /// Computes the result of summing the Fibonacci series up to the specified number.
+        /// </summary>
+        /// <param name="number">Highest number of Fibonacci series to sum.</param>
+        /// <returns>Sum of the specified number of numbers in Fibonacci series.</returns>
+        public int Generate(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("number cannot be negative", nameof(number));
+            }
+            else if (number == 0)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else if (results[number] != 0)
+            {
+                return results[number];
+            }
+            else
+            {
+                results[number] = (Generate(number - 2) + Generate(number - 1));
+                return results[number];
+            }
+        }
+
+            private readonly List<int> results = new List<int>();
+        }
+    }
+}
---
+++ FibonacciSeries/FibonacciGeneratorTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FibonacciSeries
+{
+    /// <summary>
+    /// Test for FibonacciGenerator class.
+    /// </summary>
+    [TestFixture]
+    class FibonacciGeneratorTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            generator = new FibonacciGenerator();
+        }
+
+        /// <summary>
+        /// Checks that a negative number is handled correctly.
+        /// </summary>
+        [Test]
+        public void Negative()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => generator.Generate(-1));
+
+            StringAssert.Contains("number cannot be negative", ex.Message);
+            StringAssert.Contains("number", ex.ParamName);
+        }
+
+        /// <summary>
+        /// Checks that a zero is handled correctly.
+        /// </summary>
+        [Test]
+        public void Zero()
+        {
+            Assert.AreEqual(0, generator.Generate(0));
+        }
+
+        /// <summary>
+        /// Checks that one is handled correctly.
+        /// </summary>
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual(1, generator.Generate(1));
+        }
+
+        /// <summary>
+        /// Checks that a positive numbers are handled correctly.
+        /// </summary>
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(39, 63245986)]
+        public void Positive(int number, int expected)
+        {
+            Assert.AreEqual(expected, generator.Generate(number));
+        }
+
+        /// <summary>
+        /// Fibonacci generator.
+        /// </summary>
+        private FibonacciGenerator generator;
+    }
+}
