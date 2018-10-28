b100217f28daedb90e7fb5cd5eb3cd39977278ec
Brian Egan <brianjoseph.egan@gmail.com>
2018-10-28 15:35:35 -0700

    Printing Steps
    Implementation and tests for printing steps.

---
+++ PrintingSteps/StepPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingSteps
+{
+    /// <summary>
+    /// Class to print out steps.
+    /// </summary>
+    public class StepPrinter
+    {
+        /// <summary>
+        /// Prints the specified number of steps.
+        /// </summary>
+        /// <param name="number">Number of steps to print.</param>
+        public void Print(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("number cannot be negative", nameof(number));
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    if (j <= i)
+                    {
+                        stringBuilder.Append("#");
+                    }
+                    else
+                    {
+                        stringBuilder.Append(" ");
+                    }
+                }
+                stringBuilder.AppendLine();
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
---
+++ PrintingSteps/StepPrinterTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PrintingSteps
+{
+    /// <summary>
+    /// Test for StepPrinter class.
+    /// </summary>
+    [TestFixture]
+    class StepPrinterTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            printer = new StepPrinter();
+        }
+
+        /// Checks that negative number is handled correctly.
+        /// </summary>
+        [Test]
+        public void Negative()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => { printer.Print(-1); });
+
+            StringAssert.Contains("number cannot be negative", ex.Message);
+            StringAssert.Contains("number", ex.ParamName);
+        }
+
+        /// <summary>
+        /// Checks that zero is handled correctly.
+        /// </summary>
+        [Test]
+        public void Zero()
+        {
+            printer.Print(0);
+        }
+
+        /// <summary>
+        /// Checks that positive numbers are handled correctly.
+        /// </summary>
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void Positive(int input)
+        {
+            printer.Print(input);
+        }
+
+        /// <summary>
+        /// Step printer.
+        /// </summary>
+        private StepPrinter printer;
+    }
+}
