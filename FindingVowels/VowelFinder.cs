3bfc7892cc97d4a8b3c5275e6f8b34bfb9f2ac79
Brian Egan <brianjoseph.egan@gmail.com>
2018-10-28 15:24:22 -0700

    Finding Vowels
    Implementation and tests for finding number of vowels in a string.

---
+++ FindingVowels/VowelFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingVowels
+{
+    /// <summary>
+    /// Class to find the number of vowels in a string.
+    /// </summary>
+    public class VowelFinder
+    {
+        /// <summary>
+        /// Find the number of vowels in the specified string.
+        /// </summary>
+        /// <param name="input">String to find number of vowels in.</param>
+        /// <returns>Number of vowels in the string.</returns>
+        public int Find(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input), "input cannot be null");
+            }
+
+            int result = 0;
+            foreach (char character in input)
+            {
+                switch (char.ToLower(character))
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        {
+                            result++;
+                            break;
+                        }
+                    default:
+                        {
+                            continue;
+                        }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ FindingVowels/VowelFinderTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FindingVowels
+{
+    /// <summary>
+    /// Test for VowelFinder class.
+    /// </summary>
+    [TestFixture]
+    class VowelFinderTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            finder = new VowelFinder();
+        }
+
+        /// <summary>
+        /// Checks that null is handled correctly.
+        /// </summary>
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => { finder.Find(null); });
+
+            StringAssert.Contains("input cannot be null", ex.Message);
+            StringAssert.Contains("input", ex.ParamName);
+        }
+
+        /// <summary>
+        /// Checks that correct number of vowels are found.
+        /// </summary>
+        /// <param name="input">Input string.</param>
+        /// <param name="expected">Expected number of vowels.</param>
+        [TestCase("aeiou", 5)]
+        [TestCase("AEIOU", 5)]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", 5)]
+        [TestCase("bcdfghjkl", 0)]
+        public void Positive(string input, int expected)
+        {
+            Assert.AreEqual(expected, finder.Find(input));
+        }
+
+        /// <summary>
+        /// Vowel finder
+        /// </summary>
+        private VowelFinder finder;
+    }
+}
