47517612fb50848de7c7a67f34a958a5f7213f00
Brian Egan <brianjoseph.egan@gmail.com>
2018-12-04 20:40:46 -0800

    Binary Search Trees
    Implementation and tests for binary search trees.

---
+++ BinarySearchTrees/BinarySearchTree.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTrees
+{
+    /// <summary>
+    /// Class to implement a binary search tree.
+    /// </summary>
+    public class BinarySearchTree
+    {
+        /// <summary>
+        /// Insert the specified data into the tree.
+        /// </summary>
+        /// <param name="data">Data to insert.</param>
+        public void Insert(int data)
+        {
+            if (Root == null)
+            {
+                Root = new Node(data);
+            }
+            else
+            {
+                Insert(data, Root);
+            }
+        }
+
+        /// <summary>
+        /// Checks whether the tree contains the specified data.
+        /// </summary>
+        /// <param name="data">Data to check for.</param>
+        /// <returns>True if the tree contains the data; false otherwise.s</returns>
+        public bool Contains(int data)
+        {
+            return Contains(data, Root);
+        }
+
+        /// <summary>
+        /// Checks whether the tree is a binary search tree.
+        /// </summary>
+        /// <returns>True if the tree is valid; false otherwise.</returns>
+        public bool Validate()
+        {
+            return Validate(Root, int.MinValue, int.MaxValue);
+        }
+
+        /// <summary>
+        /// Root node of the tree.
+        /// </summary>
+        public Node Root { get; set; }
+
+        /// <summary>
+        /// Recursively inserts the specified data starting at the specified node.
+        /// </summary>
+        /// <param name="data">Data to insert.</param>
+        /// <param name="node">Node to use.</param>
+        private void Insert(int data, Node node)
+        {
+            if (data < node.Data && node.Left != null)
+            {
+                Insert(data, node.Left);
+            }
+            else if (data < node.Data)
+            {
+                node.Left = new Node(data);
+            }
+            else if (data >= node.Data && node.Right != null)
+            {
+                Insert(data, node.Right);
+            }
+            else
+            {
+                node.Right = new Node(data);
+            }
+        }
+
+        /// <summary>
+        /// Recursively checks to see if the specified data is contained in the tree, starting at the specified node.
+        /// </summary>
+        /// <param name="data">Data to insert.</param>
+        /// <param name="node">Node to start at.</param>
+        /// <returns>True if the data is contained in the tree; false otherwise.</returns>
+        private bool Contains(int data, Node node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            else if (data == node.Data)
+            {
+                return true;
+            }
+            else if (data < node.Data)
+            {
+                return Contains(data, node.Left);
+            }
+            else
+            {
+                return Contains(data, node.Right);
+            }
+        }
+
+        /// <summary>
+        /// Recursively checks to see if the tree is a valid binary search tree, starting at the specified node.
+        /// </summary>
+        /// <param name="node">Node to start at.</param>
+        /// <param name="min">Minimum valid data.</param>
+        /// <param name="max">Maximum valid data.</param>
+        /// <returns>True if the tree is a valid binary search tree; false otherwise.</returns>
+        private bool Validate(Node node, int min, int max)
+        {
+            if (node.Data > max)
+            {
+                return false;
+            }
+
+            if (node.Data < min)
+            {
+                return false;
+            }
+
+            if (node.Left != null && !Validate(node.Left, min, node.Data))
+            {
+                return false;
+            }
+
+            if (node.Right != null && !Validate(node.Right, node.Data, max))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Class to implement a tree node.
+        /// </summary>
+        public class Node
+        {
+            /// <summary>
+            /// Data held by the node.
+            /// </summary>
+            public int Data { get; private set; }
+
+            /// <summary>
+            /// Node to the left.
+            /// </summary>
+            public Node Left { get; set; }
+
+            /// <summary>
+            /// Node to the right.
+            /// </summary>
+            public Node Right { get; set; }
+
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            /// <param name="data">Data to hold.</param>
+            public Node(int data)
+            {
+                Data = data;
+                Left = null;
+                Right = null;
+            }
+        }
+    }
+}
---
+++ BinarySearchTrees/BinarySearchTreeTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BinarySearchTrees
+{
+    /// <summary>
+    /// Tests for BinarySearchTree class.
+    /// </summary>
+    [TestFixture]
+    class BinarySearchTreeTests
+    {
+        /// <summary>
+        /// Set up for every test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            tree = new BinarySearchTree();
+        }
+
+        /// <summary>
+        /// Checks that Insert is implemented correctly.
+        /// </summary>
+        [Test]
+        public void Insert()
+        {
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(17);
+
+            Assert.AreEqual(5, tree.Root.Data);
+            Assert.AreEqual(15, tree.Root.Right.Data);
+            Assert.AreEqual(17, tree.Root.Right.Right.Data);
+        }
+
+        /// <summary>
+        /// Checks that Contains is implemented correctly when the binary search tree does conatin the data.
+        /// </summary>
+        [Test]
+        public void ContainsPositive()
+        {
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(20);
+            tree.Insert(0);
+            tree.Insert(-5);
+            tree.Insert(3);
+
+            Assert.IsTrue(tree.Contains(3));
+        }
+
+        /// <summary>
+        /// Checks that Contains is implemented correctly when the binary search tree does not conatin the data.
+        /// </summary>
+        [Test]
+        public void ContainsNegative()
+        {
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(20);
+            tree.Insert(0);
+            tree.Insert(-5);
+            tree.Insert(3);
+
+            Assert.IsFalse(tree.Contains(-3));
+        }
+
+        /// <summary>
+        /// Checks that Validate is implemented correctly when the binary search tree is valid.
+        /// </summary>
+        [Test]
+        public void ValidatePositive()
+        {
+            tree.Insert(10);
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(0);
+            tree.Insert(20);
+
+            Assert.IsTrue(tree.Validate());
+        }
+
+        /// <summary>
+        /// Checks that Validate is implemented correctly when the binary search tree is not valid.
+        /// </summary>
+        [Test]
+        public void ValidateNegative()
+        {
+            tree.Insert(10);
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(0);
+            tree.Insert(20);
+            tree.Root.Left.Left.Right = new BinarySearchTree.Node(999);
+
+            Assert.IsFalse(tree.Validate());
+        }
+
+        /// <summary>
+        /// Binary search tree.
+        /// </summary>
+        private BinarySearchTree tree;
+    }
+}
