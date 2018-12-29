30108995bebb158c1b313e746ce3b12812a46b38
Brian Egan <brianjoseph.egan@gmail.com>
2018-12-28 22:38:58 -0800

    Queue Stacks
    Implementation and tests for implementing a queue using stacks.

---
+++ QueueStacks/QueueStack.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stacks;
+
+namespace QueueStacks
+{
+    /// <summary>
+    /// Class to implement a queue using stacks.
+    /// </summary>
+    public class QueueStack
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public QueueStack()
+        {
+            addingStack = new Stack();
+            removingStack = new Stack();
+            mode = Mode.Adding;
+            count = 0;
+        }
+
+        /// <summary>
+        /// Adds the specified item to the back of the queue.
+        /// </summary>
+        /// <param name="item">Item to add.</param>
+        public void Add(object item)
+        {
+            if (mode == Mode.Removing)
+            {
+                FlipStacks(removingStack, addingStack);
+            }
+
+            count++;
+            addingStack.Push(item);
+        }
+
+        /// <summary>
+        /// Peeks the item in the front of the queue.
+        /// </summary>
+        /// <returns>The item at the front of the queue.</returns>
+        public object Peek()
+        {
+            if (count <= 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            if (mode == Mode.Adding)
+            {
+                FlipStacks(addingStack, removingStack);
+            }
+
+            return removingStack.Peek();
+        }
+
+        /// <summary>
+        /// Removes the item at the front of the queue.
+        /// </summary>
+        /// <returns>The item at the front of the queue.</returns>
+        public object Remove()
+        {
+            if (count <= 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            if (mode == Mode.Adding)
+            {
+                FlipStacks(addingStack, removingStack);
+            }
+
+            count--;
+            return removingStack.Pop();
+        }
+
+        /// <summary>
+        /// Adding stack.
+        /// </summary>
+        private Stack addingStack;
+
+        /// <summary>
+        /// Removing stack.
+        /// </summary>
+        private Stack removingStack;
+
+        /// <summary>
+        /// Mode.
+        /// </summary>
+        private Mode mode;
+
+        /// <summary>
+        /// Number of items in the queue.
+        /// </summary>
+        private int count;
+
+        /// <summary>
+        /// Mode
+        /// </summary>
+        private enum Mode
+        {
+            /// <summary>
+            /// Adding to the queue.
+            /// </summary>
+            Adding,
+
+            /// <summary>
+            /// Removing from the queue.
+            /// </summary>
+            Removing
+        }
+
+        /// <summary>
+        /// Flips the specified stacks, moving items from source stack to the destination stack.
+        /// </summary>
+        /// <param name="source">Source stack.</param>
+        /// <param name="destination">Destination stack.</param>
+        private void FlipStacks(Stack source, Stack destination)
+        {
+            try
+            {
+                while (true)
+                {
+                    destination.Push(source.Pop());
+                }
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+    }
+}
---
+++ QueueStacks/QueueStackTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QueueStacks
+{
+    /// <summary>
+    /// Tests for QueueStack class.
+    /// </summary>
+    [TestFixture]
+    public class QueueStackTests
+    {
+        /// <summary>
+        /// Setup for every test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            queue = new QueueStack();
+        }
+
+        /// <summary>
+        /// Checks that Add is implmeneted correctly.
+        /// </summary>
+        [Test]
+        public void Add()
+        {
+            Assert.DoesNotThrow(() => queue.Add(1));
+        }
+
+        /// <summary>
+        /// Checks that Remove is implemented correctly.
+        /// </summary>
+        [Test]
+        public void Remove()
+        {
+            queue.Add(1);
+            Assert.DoesNotThrow(() => queue.Remove());
+        }
+
+        /// <summary>
+        /// Checks that order is maintained correctly.
+        /// </summary>
+        [Test]
+        public void Order()
+        {
+            queue.Add(1);
+            queue.Add(2);
+            queue.Add(3);
+
+            Assert.AreEqual(1, queue.Remove());
+            Assert.AreEqual(2, queue.Remove());
+            Assert.AreEqual(3, queue.Remove());
+            Assert.Throws<InvalidOperationException>(() => queue.Remove());
+        }
+
+        /// <summary>
+        /// Checks that Peek is implemented correctly.
+        /// </summary>
+        [Test]
+        public void Peek()
+        {
+            queue.Add(1);
+            queue.Add(2);
+
+            Assert.AreEqual(1, queue.Peek());
+            Assert.AreEqual(1, queue.Remove());
+            Assert.AreEqual(2, queue.Peek());
+            Assert.AreEqual(2, queue.Remove());
+            Assert.Throws<InvalidOperationException>(() => queue.Peek());
+        }
+
+        /// <summary>
+        /// Queue stack.
+        /// </summary>
+        private QueueStack queue;
+    }
+}
