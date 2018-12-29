c6bf106e1b1c9a047333c5a82a6ac3d1f25f6ae0
Brian Egan <brianjoseph.egan@gmail.com>
2018-12-28 22:27:00 -0800

    Level Width
    Implementation and tests to find the width of each level of a tree.

---
+++ LevelWidth/LevelWidthFinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trees;
+
+namespace LevelWidth
+{
+    /// <summary>
+    /// Class to find the width of each level of a tree.
+    /// </summary>
+    public class LevelWidthFinder
+    {
+        /// <summary>
+        /// Finds the width of each level of the specified tree.
+        /// </summary>
+        /// <param name="tree">Tree to find width of levels.</param>
+        /// <returns>Width of levels in the tree.</returns>
+        public int[] Find(Tree tree)
+        {
+            if (tree == null)
+            {
+                throw new ArgumentNullException(nameof(tree), "tree cannot be null");
+            }
+
+            if (tree.Root == null)
+            {
+                return new int[] { 0 };
+            }
+
+            List<int> results = new List<int>();
+            List<Tree.Node> toVisit = new List<Tree.Node>();
+            toVisit.Add(tree.Root);
+
+            while (toVisit.Count > 0)
+            {
+                int levelCount = toVisit.Count;
+
+                for (int i = 0; i < levelCount; i++)
+                {
+                    Tree.Node node = toVisit[0];
+                    toVisit.RemoveAt(0);
+                    toVisit.AddRange(node.Children);
+                }
+
+                results.Add(levelCount);
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ LevelWidth/LevelWidthFinderTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trees;
+using NUnit.Framework;
+
+namespace LevelWidth
+{
+    /// <summary>
+    /// Tests for the LevelWidthFinder class.
+    /// </summary>
+    [TestFixture]
+    public class LevelWidthFinderTests
+    {
+        /// <summary>
+        /// Setup for each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            finder = new LevelWidthFinder();
+        }
+
+        /// <summary>
+        /// Checks that a null tree is handled correctly.
+        /// </summary>
+        [Test]
+        public void Null()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => finder.Find(null));
+
+            StringAssert.Contains("tree cannot be null", ex.Message);
+        }
+
+        /// <summary>
+        /// Checks that a tree with no levels finds correct widths.
+        /// </summary>
+        [Test]
+        public void NoLevels()
+        {
+            Tree tree = new Tree();
+
+            CollectionAssert.AreEqual(new int[] { 0 }, finder.Find(tree));
+        }
+
+        /// <summary>
+        /// Checks that a tree with one levels finds correct widths.
+        /// </summary>
+        [Test]
+        public void OneLevel()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Tree.Node(0);
+
+            CollectionAssert.AreEqual(new int[] { 1 }, finder.Find(tree));
+        }
+
+        /// <summary>
+        /// Checks that a tree with three levels finds correct widths.
+        /// </summary>
+        [Test]
+        public void ThreeLevels()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Tree.Node(0);
+            tree.Root.Add(1);
+            tree.Root.Add(2);
+            tree.Root.Add(3);
+            tree.Root.Children[0].Add(4);
+            tree.Root.Children[0].Add(5);
+
+            CollectionAssert.AreEqual(new int[] { 1, 3, 2 }, finder.Find(tree));
+        }
+
+        /// <summary>
+        /// Checks that a tree with four levels finds the correct widths.
+        /// </summary>
+        [Test]
+        public void FourLevels()
+        {
+            Tree tree = new Tree();
+            tree.Root = new Tree.Node(0);
+            tree.Root.Add(1);
+            tree.Root.Children[0].Add(2);
+            tree.Root.Children[0].Add(3);
+            tree.Root.Children[0].Children[0].Add(4);
+
+            CollectionAssert.AreEqual(new int[] { 1, 1, 2, 1 }, finder.Find(tree));
+        }
+
+        /// <summary>
+        /// Level width finder.
+        /// </summary>
+        private LevelWidthFinder finder;
+    }
+}
